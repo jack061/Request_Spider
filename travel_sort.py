87b79cc8e1d28ed281c2e9a6bc81a8a18ffcc2e6
lbja2 <lbja2@live.com>
2019-05-08 15:15:12 +0800

    first

---
+++ travel_sort.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
+  
+class QuNaEr():
+    def __init__(self, keyword,table, page=1):
+        self.keyword = keyword
+        self.page = page
+        self.table=table
+  
+    def qne_spider(self):
+        url = 'https://piao.qunar.com/ticket/list.htm?keyword=%s&region=&from=mpl_search_suggest&page=%s' % (self.keyword, self.page)
+        response = requests.get(url)
+        response.encoding = 'utf-8'
+        text = response.text
+        bs_obj = BeautifulSoup(text, 'html.parser')
+  
+        arr = bs_obj.find('div', {'class': 'result_list'}).contents
+        for i in arr:
+            info = i.attrs
+            # 景区名称
+            name = info.get('data-sight-name')
+            # 地址
+            address = info.get('data-address')
+            # 近期售票数
+            count = info.get('data-sale-count')
+            # 经纬度
+            point = info.get('data-point')
+  
+            # 起始价格
+            price = i.find('span', {'class': 'sight_item_price'})
+            price = price.find_all('em')
+            price = price[0].text
+            self.table.insert_one({
+                'name'      :   name,
+                'address'   :   address,
+                'count'     :   int(count),
+                'point'     :   point,
+                'price'     :   float(price),
+                'city'      :   self.keyword
+            })
+  
+if __name__ == '__main__':
+    citys = ['北京', '上海', '成都', '三亚', '广州', '重庆', '深圳', '西安', '杭州', '厦门', '武汉', '大连', '苏州']
+    conn = MongoClient('localhost', port=27017)
+    db = conn.QuNaEr # 库
+    table = db.qunaer_51  # 表
+    #清除数据
+    table.remove({})
+    for i in citys:
+        for page in range(1, 5):
+            qne = QuNaEr(i,table, page=page)
+            qne.qne_spider()
